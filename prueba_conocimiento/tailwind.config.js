e7d93cb545c9bab2dea774322e6b4180f7fb3a31
AndresRodas <56941654+AndresRodas@users.noreply.github.com>
2022-08-15 03:01:29 -0600

    Tailwind & DaisyUI added!

---
+++ prueba_conocimiento/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'), 
+    require('@tailwindcss/forms'), 
+    require('@tailwindcss/line-clamp'), 
+    require('@tailwindcss/typography'),
+    require("daisyui"),
+  ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  }
+};
