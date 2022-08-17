a34e824fc9859dbbab4dc7378129212c97cdfa16
AndresRodas <56941654+AndresRodas@users.noreply.github.com>
2022-08-17 02:00:24 -0600

    Profile card, first version

--- prueba_conocimiento/src/app/profile/profile.page.ts
+++ prueba_conocimiento/src/app/profile/profile.page.ts
@@ -8,10 +8,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfilePage implements OnInit {
   public folder: string;
+  public user: string = 'Juan Perez';
+  private dateFormat: Date = new Date();
+  public date: string;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.date = `${this.dateFormat.getDate()}/${this.dateFormat.getMonth()+1}/${this.dateFormat.getFullYear()}`;
   }
 }
